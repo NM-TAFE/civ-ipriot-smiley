a18e833daa62d32d2529685c6c7db3bf884dddbb
rafael <rafael.avigad@nmtafe.wa.edu.au>
2023-04-25 19:37:18 +0800

    Fixed a bug with the low_light property that could cause a crash in some circumstances

--- sense_hat.py
+++ sense_hat.py
@@ -19,6 +19,7 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s - %(linen
 class SenseHat:
     def __init__(self, background=DEFAULT_RGB):
         self.logger = logging.getLogger(__name__)
+        self._low_light = False
         self.logger.info('Starting mock SenseHAT')
         self.queue = mp.Queue()
         self.process = mp.Process(target=self.run_hat_gui, args=(background,))
@@ -30,7 +31,7 @@ class SenseHat:
     def run_hat_gui(self, initial_rgb_values):
         self.logger.debug('Starting mock SenseHAT GUI')
         self.rgb_values = initial_rgb_values
-        self._low_light = False
+        # self._low_light = False
 
         self.root = tk.Tk()
         self.root.title('Mock SenseHAT')
@@ -45,7 +46,7 @@ class SenseHat:
         self.root.mainloop()
 
     def refresh_gui_from_queue(self):
-        self._set_pixels(self.rgb_values)
+        # self._set_pixels(self.rgb_values)
         with contextlib.suppress(queue.Empty):
             message, payload = self.queue.get_nowait()
             if message == 'set_pixels':
@@ -64,13 +65,14 @@ class SenseHat:
 
     def _set_pixels(self, rgb_values):
         self.logger.debug('Setting mock SenseHAT LED matrix pixel values')
+        self.logger.debug(f'{self.low_light=}')
         if self.low_light:
-            dimming_factor = 0.2  # Adjust this value between 0 and 1 for different dimming levels
+            dimming_factor = 0.6  # Adjust this value between 0 and 1 for different dimming levels
             rgb_values = [(int(r * dimming_factor), int(g * dimming_factor),
                            int(b * dimming_factor)) for r, g, b in rgb_values]
 
         self.rgb_values = rgb_values
-
+        self.logger.debug(f'{self.rgb_values=}')
         for i, rgb in enumerate(rgb_values):
             self.led_matrix[i].config(bg='#%02x%02x%02x' % rgb)  # type: ignore
 
@@ -80,7 +82,7 @@ class SenseHat:
 
     @property
     def low_light(self):
-        return False
+        return self._low_light
 
     @low_light.setter
     def low_light(self, value):
@@ -97,13 +99,25 @@ def main():
     mock_sense_hat = SenseHat()
     mock_sense_hat.set_pixels(rgb_values)
 
-    time.sleep(5)
+    time.sleep(1)
     mock_sense_hat.set_pixels([(0, 255, 0)] * 64)
-    time.sleep(2)
+    time.sleep(1)
     mock_sense_hat.set_pixels([(0, 0, 255)] * 64)
     # To enable low_light mode, set the low_light attribute to True
     mock_sense_hat.low_light = True
+    time.sleep(1)
+    mock_sense_hat.set_pixels(rgb_values)
+    time.sleep(1)
+    mock_sense_hat.set_pixels([(0, 255, 0)] * 64)
+    time.sleep(1)
+    mock_sense_hat.set_pixels([(0, 0, 255)] * 64)
     mock_sense_hat.low_light = False
+    time.sleep(1)
+    mock_sense_hat.set_pixels(rgb_values)
+    time.sleep(1)
+    mock_sense_hat.set_pixels([(0, 255, 0)] * 64)
+    time.sleep(1)
+    mock_sense_hat.set_pixels([(0, 0, 255)] * 64)
 
     # Keep the main thread alive
     pause()
