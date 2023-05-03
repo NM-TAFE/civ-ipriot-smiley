a5ebcc1f35a5eefc10747243b8203097518ac9e6
Lucas Fadersen <v092953@tafe.wa.edu.au>
2023-05-03 16:49:53 +0800

    class end

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ That is, delete the sense_hat.py file that is included in this bundle."""
 import time
 
 from happy import Happy
+from sad import Sad
 
 if __name__ == '__main__':
     # This is only needed if you have not deleted sense_hat.py
@@ -30,3 +31,11 @@ if __name__ == '__main__':
     # is defined as an interface (i.e., an abstract base class
     # with an abstract method).
     smiley.blink()
+
+    sad = Sad()
+    sad.show()
+    time.sleep(1)
+    smiley.blink()
+
+
+
