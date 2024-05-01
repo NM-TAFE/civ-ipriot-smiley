00bc6aa50b2a1b870f92b9650be22bc70e89db16
20125282 <20125282@tafe.wa.edu.au>
2024-05-01 16:53:30 +0800

    Make sad blink

--- sad.py
+++ sad.py
@@ -1,7 +1,9 @@
+import time
+from blinkable import Blinkable
 from smiley import Smiley
 
 
-class Sad(Smiley):
+class Sad(Smiley, Blinkable):
     def __init__(self):
         super().__init__()
 
@@ -24,3 +26,10 @@ class Sad(Smiley):
         eyes = [10, 13, 18, 21]
         for pixel in eyes:
             self.pixels[pixel] = self.BLANK if wide_open else self.YELLOW
+
+    def blink(self, delay=0.25):
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
