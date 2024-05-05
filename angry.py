3f50d5adc816752e6226f5d1ed7ae3b92bfbc267
20125282 <20125282@tafe.wa.edu.au>
2024-05-05 12:23:09 +0800

    New subclass angry smiley

---
+++ angry.py
@@ -0,0 +1,35 @@
+import time
+from blinkable import Blinkable
+from smiley import Smiley
+
+
+class Angry(Smiley, Blinkable):
+    def __init__(self):
+        super().__init__(complexion=self.RED)
+
+        self.draw_mouth()
+        self.draw_eyes()
+
+    def draw_mouth(self):
+        """
+        Method that draws the mouth on the standard faceless smiley.
+        """
+        mouth = [49, 54, 42, 43, 44, 45]
+        for pixel in mouth:
+            self.pixels[pixel] = self.BLANK
+
+    def draw_eyes(self, wide_open=True):
+        """
+        Method that draws the eyes (open or closed) on the standard smiley.
+        :param wide_open: True if eyes opened, False otherwise
+        """
+        eyes = [9, 14, 18, 21]
+        for pixel in eyes:
+            self.pixels[pixel] = self.BLANK if wide_open else self.RED
+
+    def blink(self, delay=0.25):
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
