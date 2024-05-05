ecefc2344c125056827f5b414fded70e611872b9
20125282 <20125282@tafe.wa.edu.au>
2024-05-05 11:37:23 +0800

    Added instance variable "my_complexion"

--- smiley.py
+++ smiley.py
@@ -8,12 +8,16 @@ class Smiley:
     YELLOW = (255, 255, 0)
     BLANK = (0, 0, 0)
 
-    def __init__(self):
+    def __init__(self, complexion=None):
         # We have encapsulated the SenseHat object
         self.sense_hat = SenseHat()
 
+        if complexion is None:
+            complexion = self.YELLOW
 
-        Y = self.complexion()
+        self.my_complexion = complexion
+
+        Y = self.my_complexion
         O = self.BLANK
         self.pixels = [
             O, Y, Y, Y, Y, Y, Y, O,
@@ -43,4 +47,4 @@ class Smiley:
         """
         Returns the complexion of the smiley.
         """
-        return self.YELLOW
+        return self.my_complexion
