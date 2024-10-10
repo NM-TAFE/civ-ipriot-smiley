98366ab1f8434f615896400d1b96ce0b66601ed0
Rafael A. <30427560+rafrafavi@users.noreply.github.com>
2024-10-10 18:39:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,5 +21,5 @@ if __name__ == '__main__':
     # from multiprocessing import freeze_support
     # freeze_support()
     ############################################################
-
+    main()
 
