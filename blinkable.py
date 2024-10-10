636904c32b54dad413e0b045b32fd15ff642620c
Rafael A. <30427560+rafrafavi@users.noreply.github.com>
2024-10-10 18:32:15 +0800

    Fix errors in blinkable docstring

--- blinkable.py
+++ blinkable.py
@@ -3,10 +3,7 @@ from abc import ABC, abstractmethod
 
 class Blinkable(ABC):
     """
-    We can use an Abstract Base Class (ABC) to create an interface.
-    The interface is an abstract method. It has not real implementation,
-    it can simply contain pass. It must, however, by imlemented
-    by subclasses that promise to implement it.
+    Specify what anything that claims to be 'blinkable' should be able to do.
     """
     @abstractmethod
     def blink(self):
